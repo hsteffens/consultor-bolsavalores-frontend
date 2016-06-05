66bfa4a93aa3b3a749337fc96470ec4a88ebb912
Helinton Steffens <helinton.pereira@hotmail.com>
2016-06-05 18:35:13 -0300

    #16 - Criado metodo para autenticação via JWT.

---
+++ consultor-frontend/WebContent/bower_components/urban-notification-bar/HISTORY.md
@@ -0,0 +1,21 @@
+## 0.2.2 - 15.09.2014
+
+* _fix_ - remove anims leaking to global
+
+## 0.2.1 - 14.09.14
+
+* _fix_ - explicitly declare bezier-easing dependency
+
+## 0.2.0 - 14.09.14
+
+* _add_ - smooth out delayed animations
+
+## 0.1.1 - 12.09.14
+
+* _fix_ - stop multiple hides from starting fresh animations
+* _fix_ - mobile friendly notifications
+
+## 0.1.0 - 11.09.14
+
+* _add_ - sliding bar and content
+* _add_ - concertina animation
---
+++ consultor-frontend/WebContent/bower_components/urban-notification-bar/urban-notification-bar.js
@@ -0,0 +1,175 @@
+(function( root ) {
+
+    var ANIM_OUT_SPD = 200,
+        ANIM_IN_SPD = 400,
+        ANIM_DELAY_MAG = 50,
+        ANIM_EASING = 'cubic-bezier( .55, .87, .55, .92 )';
+
+
+    Polymer( 'urban-notification-bar', {
+
+        /**
+         * The cached total length of the animation, usually this will be determined by the number of content roots to be animated
+         *
+         * @type {Integer}
+         */
+        animationDuration: ANIM_DELAY_MAG,
+
+
+        /**
+         * Published properties
+         */
+        publish: {
+            /**
+             * Is the element showing
+             *
+             * @property _showing
+             * @type boolean
+             */
+            _showing: false
+        },
+
+
+        /*-----------------------------------------------------------*\
+         *
+         *  Polymer lifecycle events
+         *
+        \*-----------------------------------------------------------*/
+
+        /**
+         * Fired when Polymer has got the element ready
+         */
+        ready: function() {
+            this.super();
+        },
+
+
+        /*-----------------------------------------------------------*\
+         *
+         *  State Management
+         *
+        \*-----------------------------------------------------------*/
+
+
+        /**
+         * Shows the whole element and starts the 'in' animation
+         *
+         * @param animate {Boolean} determines if the animation fires or not
+         * @event - emits a 'show' event
+         */
+        show: function( immediate ) {
+            if ( this._showing ) return;
+
+            var anims = [];
+            anims.push( new Animation(
+                this,
+                this.frames.in, {
+                    duration: ANIM_IN_SPD,
+                    fill: 'forwards',
+                    easing: ANIM_EASING
+                }
+            ));
+
+            this.contents.forEach( function( el, index ) {
+                anims.push( new Animation(
+                    el,
+                    this.frames.show, {
+                        duration: ANIM_IN_SPD,
+                        delay: this.$.bezier.calc( ( index + 1 ) / this.contents.length ) * this.animationDuration,
+                        fill: 'forwards',
+                        easing: ANIM_EASING
+                    }
+                ));
+            }, this );
+
+            var anim = document.timeline.play( new AnimationGroup( anims ) );
+
+            anim.onfinish = function( event ) {
+                this.fire( 'showEnd' );
+            }.bind( this );
+
+            this._showing = true;
+            this.fire( 'showStart' );
+        },
+
+
+        /**
+         * Hides the whole element and starts the 'out' animation
+         *
+         * @event - emits a 'hide' event
+         */
+        hide: function() {
+            if ( !this._showing ) return;
+
+            var anims = [];
+            this.contents.forEach( function( el, index ) {
+                anims.push( new Animation(
+                    this.contents[ this.contents.length - index - 1 ],
+                    this.frames.hide, {
+                        duration: ANIM_OUT_SPD,
+                        delay: this.$.bezier.calc( ( index + 1 ) / this.contents.length ) * this.animationDuration,
+                        fill: 'forwards',
+                        easing: ANIM_EASING
+                    }
+                ));
+            }, this );
+
+            anims.push( new Animation(
+                this,
+                this.frames.out, {
+                    duration: ANIM_OUT_SPD,
+                    delay: ANIM_OUT_SPD * .8,
+                    fill: 'forwards',
+                    easing: ANIM_EASING
+                }
+            ));
+
+            var anim = document.timeline.play( new AnimationGroup( anims ) );
+            this._showing = false;
+            this.fire( 'hideStart' );
+
+            // Last to start will be last to finish so listen for the last one
+            anim.onfinish = function( event ) {
+                this.fire( 'hideEnd' );
+            }.bind( this );
+        },
+
+
+        /*-----------------------------------------------------------*\
+         *
+         *  Helpers
+         *
+        \*-----------------------------------------------------------*/
+
+        /**
+         * Creates the animation frames
+         */
+        createAnimationFrames: function() {
+            this.frames = {
+                show: new KeyframeEffect([
+                    { opacity: '0', transform: 'translate3d( 0, -20, 0 )' },
+                    { opacity: '1', transform: 'translate3d( 0, 0, 0 )' }
+                ]),
+
+                hide: new KeyframeEffect([
+                    { opacity: '1', transform: 'translate3d( 0, 0, 0 )' },
+                    { opacity: '0', transform: 'translate3d( 0, -10, 0 )' }
+                ]),
+
+                // Controls the actual bar sliding in and out
+                in: new KeyframeEffect([
+                    { transform: 'translate3d(0,-100%,0)' },
+                    { transform: 'translate3d(0,0,0)' }
+                ]),
+
+                out: new KeyframeEffect([
+                    { transform: 'translate3d(0,0,0)' },
+                    { transform: 'translate3d(0,-100%,0)' }
+                ])
+            };
+        }
+
+    });
+
+
+})( this );
